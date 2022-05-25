fcdef6b37f0739655bfde5929ddaef07813f9603
DinosDev <erick@bilhalba.com.br>
2022-05-25 00:54:56 -0300

    feat(RC-04 03): Add Paragraph

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -15,10 +15,21 @@ const Title = styled.h1`
     margin-bottom: 0.5rem;
 `
 
+const Text = styled.p`
+    text-align: center;
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+`
+
+const Bold = styled.span`
+    font-weight: bold;
+`
+
 export default function Introduction() {
     return (
         <Container>
             <Title>The Formula 1 Blog Open Source</Title>
+            <Text>Do you Like Formula 1 And Coding? So You will love <Bold>Racing Coffe</Bold></Text>
         </Container>
     )
 }
