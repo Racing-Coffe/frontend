0c1153a058d796c1a5b86bc473818b4611de3598
DinosDev <erick@bilhalba.com.br>
2022-05-25 00:51:44 -0300

    feat(RC-04 02): Add Main Text

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,10 +1,24 @@
 import styled from "styled-components"
 
-const Container = styled.div``
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    background-color: #fff;
+`
+
+const Title = styled.h1`
+    text-align: center;
+    font-size: 2rem;
+    margin-bottom: 0.5rem;
+`
 
 export default function Introduction() {
     return (
         <Container>
+            <Title>The Formula 1 Blog Open Source</Title>
         </Container>
     )
 }
